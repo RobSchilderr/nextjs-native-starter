25449b355d823ba93733820f4c838f7b80fc6c14
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-01-07 20:57:32 +0000

    fix build?

--- apps/next-app/src/pages/_app.tsx
+++ apps/next-app/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useEffect } from 'react'
 
 import { AppProps } from 'next/app'
--- apps/next-web/src/pages/_app.tsx
+++ apps/next-web/src/pages/_app.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import { AppProps } from 'next/app'
