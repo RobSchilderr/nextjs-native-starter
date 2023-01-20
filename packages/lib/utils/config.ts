a7b52cb8d3c98b599fa8b3774b760eb154f54e89
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-01-20 11:08:46 +0000

    add prod url and redirect url

--- packages/lib/utils/config.ts
+++ packages/lib/utils/config.ts
@@ -1,4 +1,4 @@
-export const PROD_URL_NO_HTTPS = 'yourprodurl.com'
+export const PROD_URL_NO_HTTPS = 'next-capacitor-supertokens.app'
 
 export const IS_IN_PRODUCTION_ENVIRONMENT =
   process.env.NEXT_PUBLIC_APP_STAGE !== 'development'
@@ -11,7 +11,7 @@ export const APP_BUNDLE_URL = `next.supertokens.app` // used to create the deep 
 
 export const REDIRECT_URL = !IS_IN_PRODUCTION_ENVIRONMENT
   ? FRONTEND_URL
-  : 'https://yourprodurl.vercel.app'
+  : 'https://capacitor-supertokens-nextjs-turborepo-next-web-one.vercel.app'
 
 export const APP_ORIGIN_URLS = [
   'capacitor://localhost',
