da280905f62fb357444c28a1c0d3c525db0401c2
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-08-15 14:35:49 +0200

    fix: build

--- apps/next-web/src/pages/_app.tsx
+++ apps/next-web/src/pages/_app.tsx
@@ -1,15 +1,13 @@
-
 /* eslint-disable import/no-extraneous-dependencies */
 // @ts-nocheck
-
 import React from 'react'
 import { AppProps } from 'next/app'
 import '../../styles/globals.css'
 import SuperTokens from 'supertokens-web-js'
-import { getFrontendConfig } from 'next-web/config/frontendConfig'
 import { Bebas_Neue, Work_Sans } from '@next/font/google'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from 'lib/utils/reactQuery'
+import { getFrontendConfig } from 'next-web/config/frontendConfig'
 
 const workSans = Work_Sans({
   subsets: ['latin'],
@@ -31,18 +29,17 @@ if (typeof window !== 'undefined') {
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <QueryClientProvider client={queryClient}>
-
-  <main className={`${bebasNeue.variable} font-display`}>
-    <div className={`${workSans.variable} font-sans`}>
-      {/* // eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component
-        {
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          ...pageProps
-        }
-      />
-    </div>
-  </main>
+    <main className={`${bebasNeue.variable} font-display`}>
+      <div className={`${workSans.variable} font-sans`}>
+        {/* // eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            ...pageProps
+          }
+        />
+      </div>
+    </main>
   </QueryClientProvider>
 )
 
