b08a334af4f52ad1e69c0b6527575c2e51040c3d
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-09-16 21:25:34 +0100

    fix: put back login result on app

--- packages/ui/components/Hero.tsx
+++ packages/ui/components/Hero.tsx
@@ -41,7 +41,7 @@ export function Hero() {
         <button
           type="button"
           onClick={() => {
-            router.push('/home')
+            router.push('/login-result')
           }}
           className="px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-gray-300 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
         >
