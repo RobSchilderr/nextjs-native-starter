0c14a8f03fc38e2d0d57573873d96356b71349a7
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-10-10 12:47:03 +0200

    chore: share js dependencies and update readme

--- apps/next-app/capacitor.config.ts
+++ apps/next-app/capacitor.config.ts
@@ -4,11 +4,16 @@ const config: CapacitorConfig = {
   appId: 'next.supertokens.app',
   appName: 'next-app-supertokens',
   webDir: 'out',
-  // For live reload: comment out server. Issue: Auth stops working.
+  // For live reload: comment out server.
   // server: {
   //   url: 'http://YOUR_IP:3001',
   // },
   bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true, // Set this to false to maintain splash screen on screen and fetch data for example on startup
+    },
+  },
 }
 
 export default config
