6b0609d7b138e2b9e3faa882758cd59a34b5c664
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-10-14 09:47:45 +0200

    chore: update turborepo and new PROD_URL_NO_HTTPS

--- packages/lib/utils/config.ts
+++ packages/lib/utils/config.ts
@@ -1,6 +1,6 @@
 import { AuthMode } from './common.types'
 
-export const PROD_URL_NO_HTTPS = 'nextjs-native.dev'
+export const PROD_URL_NO_HTTPS = 'nextjs-native-starter-app.vercel.app'
 
 export const IS_IN_PRODUCTION_ENVIRONMENT =
   process.env.NEXT_PUBLIC_APP_STAGE !== 'development'
@@ -14,7 +14,7 @@ export const APP_BUNDLE_URL = `next.supertokens.app` // used to create the deep 
 
 export const REDIRECT_URL = !IS_IN_PRODUCTION_ENVIRONMENT
   ? FRONTEND_URL
-  : 'https://nextjs-native-git-main-robschilderr.vercel.app'
+  : 'https://nextjs-native-starter-app-git-main-robschilderr.vercel.app'
 
 export const APP_ORIGIN_URLS = [
   'capacitor://localhost',
